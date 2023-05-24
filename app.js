9b789272098fb9d263a2debc8b8678bf511d4aa1
Yegon Kiprotich <meshcode75@gmail.com>
2023-05-24 19:47:35 +0300

    still tyring to solve cors error

--- app.js
+++ app.js
@@ -137,13 +137,32 @@ app.use(express.urlencoded({extended:false}))
 app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs")
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
+// app.all('/', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next()
+// });
 
   
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://easyclickspmsclient.vercel.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 //Cors 
 app.use(cors(corsOptions))
 
