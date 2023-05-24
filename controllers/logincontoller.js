64b6d045be61f89c120f092430b66aba0d14fab8
Yegon Kiprotich <meshcode75@gmail.com>
2023-05-24 20:43:26 +0300

    still tyring to solve cors error

--- controllers/logincontoller.js
+++ controllers/logincontoller.js
@@ -16,24 +16,6 @@ const secret=authconfig.secret
 
 exports.login_form_post=function(req,res,next){
     
-
-          // Website you wish to allow to connect
-          res.setHeader('Access-Control-Allow-Origin', 'https://easyclickspmsclient.vercel.app/o/auth/login/');
-        
-          // Request methods you wish to allow
-          res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        
-          // Request headers you wish to allow
-          res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        
-          // Set to true if you need the website to include cookies in the requests sent
-          // to the API (e.g. in case you use sessions)
-          //res.setHeader('Access-Control-Allow-Credentials', true);
-          
-          // Pass to next layer of middleware
-          next();
-    
-
     User.findOne({
         email:req.body.email
     }).exec().then(user=>{
