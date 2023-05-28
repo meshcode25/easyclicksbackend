89f6688c96eee8f3df4e384b27628c30dfc62dd5
Yegon Kiprotich <meshcode75@gmail.com>
2023-05-28 13:36:50 +0300

    now working on login of the already hosted backend andfront end and without cors error, Hurraygit add .

--- app.js
+++ app.js
@@ -191,6 +191,11 @@ app.use(express.urlencoded({extended:false}))
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
   console.log("scheisse");
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
