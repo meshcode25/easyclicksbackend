eea42e70b11c8712e7420dc79bdc537ed0812ea0
Yegon Kiprotich <meshcode75@gmail.com>
2023-05-28 12:24:33 +0300

    now working on login of the already hosted backend andfront end and without cors error, Hurraygit add .

--- controllers/logincontoller.js
+++ controllers/logincontoller.js
@@ -16,6 +16,7 @@ const secret=authconfig.secret
 
 exports.login_form_post=function(req,res,next){
     
+    console.log(req.body.email)
     User.findOne({
         email:req.body.email
     }).exec().then(user=>{
