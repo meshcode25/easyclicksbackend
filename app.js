af6b585d01364be65a1f145a2c8e4177c25b5163
Yegon Kiprotich <meshcode75@gmail.com>
2023-05-28 21:24:43 +0300

    now working on login of the already hosted backend andfront end and without cors error, Hurraygit add .

--- app.js
+++ app.js
@@ -130,28 +130,10 @@ app.set("view engine", "ejs")
 
   
 
-//Cors 
-//app.use(cors(corsOptions)) 
+Cors 
+app.use(cors(corsOptions)) 
 
-// // Add headers before the routes are defined
-// app.all(function (req, res, next) {
-  
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'https://easyclickspmsclient.vercel.app');
 
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   //res.setHeader('Access-Control-Allow-Credentials', true);
-  
-//   // Pass to next layer of middleware
-//   next();
-// });
 
 /*
 const index=()=>{
@@ -186,15 +168,39 @@ const index=()=>{
 
 
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  console.log("scheisse");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  console.log("scheisse");
-  next();
+// app.use((req, res, next) => {
+//   console.log("scheisse");
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   console.log("scheisse");
+//   next();
+// });
+
+// Add headers before the routes are defined
+app.all(function (req, res, next) {
+  
+ // Website you wish to allow to connect
+ res.setHeader('Access-Control-Allow-Origin', 'https://easyclickspmsclient.vercel.app');
+
+ // Request methods you wish to allow
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+ // Request headers you wish to allow
+ res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+ // Set to true if you need the website to include cookies in the requests sent
+ // to the API (e.g. in case you use sessions)
+ res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+ res.header(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept"
+);
+ // Pass to next layer of middleware
+ next();
 });
 
 //body parser and urlencode
